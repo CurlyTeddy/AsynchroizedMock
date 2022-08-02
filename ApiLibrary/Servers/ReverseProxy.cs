658899519260c59375d3532eaca016d01200b171
Teddy <teddy552@ymail.com>
2022-08-02 15:23:24 +0800

    fix: Fix the thread-safe issue of reverse proxy
    Problems:
    1. Removing an element in a collection might incur index error in other thread
    2. It's not logical that some threads are trying to remove an element that is not in the collection
    3. The selected server shouldn't be the index of the list
    
    Solutions:
    1. Expend the servers' number, then the overheads of calculation in the semaphore reduce
    2. No remove and add action are involved, so a thread-safe condition is promised

--- ApiLibrary/Servers/ReverseProxy.cs
+++ ApiLibrary/Servers/ReverseProxy.cs
@@ -1,45 +1,32 @@
-﻿namespace CustomReportLibrary
+﻿using System.Collections.Concurrent;
+
+namespace CustomReportLibrary
 {
     public class ReverseProxy : ICustomReportService
     {
         private List<ServerType> servers;
+        private ConcurrentQueue<ICustomReportService> serviceQueue;
         private SemaphoreSlim semaphore;
-        private SynchronizedCollection<int> availableServers;
         private Random random;
 
         public ReverseProxy(IEnumerable<ServerType> servers)
         {
             this.servers = servers.ToList();
-            availableServers = new SynchronizedCollection<int>();
             random = new Random();
 
-            int totalServer = 0;
-            for (int i = 0; i < this.servers.Count; ++i)
-            {
-                totalServer += this.servers[i].Limit;
-                availableServers.Add(i);
-            }
+            int totalServer = this.servers.Select(server => server.Limit).Sum();
+            List<ICustomReportService> serverResource = servers.SelectMany(server => Enumerable.Repeat(server.Server, server.Limit))
+                .OrderBy(_ => random.Next()).ToList();
+            serviceQueue = new ConcurrentQueue<ICustomReportService>(serverResource);
             semaphore = new(0, totalServer);
         }
 
         public async Task<ResponseContent> CustomReportPostAsync(RequestContent content)
         {
             await semaphore.WaitAsync();
-            int selectedServer = random.Next(0, availableServers.Count);
-
-            while (servers[selectedServer].Decrease() < 0)
-            {
-                availableServers.Remove(selectedServer);
-                servers[selectedServer].Increase();
-                selectedServer = random.Next(0, availableServers.Count);
-            }
-            
-            ResponseContent response = await servers[selectedServer].Server.CustomReportPostAsync(content);
-            servers[selectedServer].Increase();
-            if (!availableServers.Contains(selectedServer))
-            {
-                availableServers.Add(selectedServer);
-            }
+            serviceQueue.TryDequeue(out ICustomReportService? service);
+            ResponseContent response = await service!.CustomReportPostAsync(content);
+            serviceQueue.Enqueue(service);
             semaphore.Release();
 
             return response;
